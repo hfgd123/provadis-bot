be23966dcecca4cb8561be822b9cd7a01759855f
hfgd <46094961+hfgd123@users.noreply.github.com>
2024-11-05 16:24:14 +0100

    added status

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, ActivityType } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, readyClient => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+    client.user.setPresence({
+        activities: [{ name: `Provadis Stundenplan`, type: ActivityType.Watching }],
+        status: 'online',
+    });
 });
 
 client.login(token);
