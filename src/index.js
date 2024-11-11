85349107d85d6f8082498fb6e1849ce6fa6b32d6
RobertCcs <rchd@gmx.net>
2024-11-11 10:42:35 +0100

    Relocated file and sources

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 const { Client, Events, GatewayIntentBits, ActivityType } = require('discord.js');
-const { token } = require('./config.json');
+const { token } = require('./configs/Credentials.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, readyClient => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+
     client.user.setPresence({
         activities: [{ name: `Provadis Stundenplan`, type: ActivityType.Watching }],
         status: 'online',
     });
 });
 
-client.login(token);
+client.login(token.DISCORD);
